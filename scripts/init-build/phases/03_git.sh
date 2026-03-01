0199d36d1006e77f964543d0cf38c1d83ed3df9a
David Mullins <me@davit.ie>
2026-03-01 22:52:55 +0100

    fix: missing .env ignore

--- scripts/init-build/phases/03_git.sh
+++ scripts/init-build/phases/03_git.sh
@@ -1,11 +1,17 @@
+#!/usr/bin/env bash
+# Part of init-build library
+# TODO: fix the issue incorrest formating, USE gitignore.sh with OPTIONS
+#  
 git_init
 
 cat > .gitignore << 'EOF'
 node_modules/
 dist/
 archives/
+.env   # must add this
 .env.local
 .init-build/
+.bootstrap/
 EOF
 
 git_first_commit
