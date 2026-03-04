92e632c1f3407a6482bea3751366458d0ef3d5e4
David Mullins <me@davit.ie>
2026-03-04 10:13:40 +0100

    fix: update with gitignore install

--- scripts/init-build/phases/03_git.sh
+++ scripts/init-build/phases/03_git.sh
@@ -2,16 +2,6 @@
 # Part of init-build library
 # TODO: fix the issue incorrest formating, USE gitignore.sh with OPTIONS
 #  
-git_init
-
-cat > .gitignore << 'EOF'
-node_modules/
-dist/
-archives/
-.env   # must add this
-.env.local
-.init-build/
-.bootstrap/
-EOF
-
-git_first_commit
+gitignore   # check for gitignore file and install if missing
+git_init    # Git Initialise
+git_first_commit  # Commit
--- scripts/init-build/utils/git.sh
+++ scripts/init-build/utils/git.sh
@@ -1,3 +1,37 @@
+#!/usr/bin/env bash
+# git.sh
+# Version: 1.1
+# Part of init-build library
+# TODO: fix the issue incorrest formating, USE gitignore.sh with OPTIONS
+#  
+gitignore(){
+  if [[ ! -f .gitignore ]]; then
+    log_info "No .gitignore found, created new one"
+    cp .bootstrap/.gitignore .
+    log_success "'.gitignore' installed"
+  fi
+  log_info "Settings in '.gitignore' may need updating"
+
+
+cat >> .gitignore << 'EOF'
+node_modules/
+dist/
+archives/
+.env   # must add this
+.env.local
+.init-build/
+.bootstrap/
+EOF
+
+# Remove duplicates and blank lines
+tmp=".gitignore.tmp"
+echo "# revised gitignignore $(date '+%Y-%m-%d %H:%M')" > $tmp
+awk '!seen[$0]++' .gitignore | sed '/^$/N;/^\n$/D' >> $tmp
+mv .gitignore.tmp .gitignore
+log_info "Merged template and removed duplicates"
+
+}
+
 git_init() {
   if [[ ! -d .git ]]; then
     git init
